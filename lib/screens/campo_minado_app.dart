5b8a33f16433805a9150869916bad880d42e74f4
Lucas Galdino <lucasgaldino7@gmail.com>
2021-11-05 13:33:22 -0300

    5/11

--- lib/screens/campo_minado_app.dart
+++ lib/screens/campo_minado_app.dart
@@ -1,17 +1,43 @@
+import 'package:campo_minado/models/tabuleiro.dart';
 import 'package:flutter/material.dart';
+import '../components/resultado_widget.dart';
+import '../components/tabuleiro_widget.dart';
+import '../models/campo.dart';
+import '../models/explosao_exception.dart';
 
 class CampoMinadoApp extends StatelessWidget {
   const CampoMinadoApp({Key? key}) : super(key: key);
+
+  void _reiniciar() {
+    print('Reiniciar...');
+  }
+
+  void _abrir(Campo campo) {
+    print('Abrir...');
+  }
+
+  void _alternarMarcacao(Campo campo) {
+    print('Alternar marcação...');
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Campo Minado'),
+        appBar: ResultadoWidget(
+          venceu: false,
+          onReiniciar: _reiniciar,
         ),
         body: Container(
-          child: const Text('Tabuleiro'),
+          child: TabuleiroWidget(
+            tabuleiro: Tabuleiro(
+              linhas: 15,
+              colunas: 15,
+              qtdeBombas: 10,
+            ),
+            onAbrir: _abrir,
+            onAlternarMarcacao: _alternarMarcacao,
+          ),
         ),
       ),
     );
