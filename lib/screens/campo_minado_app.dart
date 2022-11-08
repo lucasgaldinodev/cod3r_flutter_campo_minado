c6b97bba7fee3e799170ab2f7c985243e0620cf2
Lucas Galdino <lucasgaldino7@gmail.com>
2022-11-08 09:31:36 -0300

    442_componente_resultado

--- lib/screens/campo_minado_app.dart
+++ lib/screens/campo_minado_app.dart
@@ -1,14 +1,20 @@
+import 'package:campo_minado/components/resultado_widget.dart';
 import 'package:flutter/material.dart';
 
 class CampoMinadoApp extends StatelessWidget {
   const CampoMinadoApp({super.key});
 
+  _reiniciar() {
+    print('reiniciar...');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Campo Minado'),
+        appBar: ResultadoWidget(
+          venceu: false,
+          onReiniciar: _reiniciar,
         ),
         body: const SizedBox(
           child: Text('Tabuleiro'),
